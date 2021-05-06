157cbd294b0613047ce4fca63db54a2f9e4f96b7
RobKlock <rklock@oberlin.edu>
2021-05-06 00:30:49 -0400

    different hardcoded weights is functional

--- write-weight-matrix.py
+++ write-weight-matrix.py
@@ -10,17 +10,20 @@ date_time = cur_date.strftime("%m-%d-%Y-%H-%M-%S")
 fi = "weighted-grid-tsp-{date}.tsp"
 filename = fi.format(date=date_time)
 f = open(filename, "x")
-DIMENSION = 10
+DIMENSION = 100
 f.write("""NAME: distance-matrix-tsp\n
 TYPE: TSP\n
 COMMENT: Test for distance matrix using Linkern algorithm (Klock and Cappelletti)\n
-DIMENSION: 10\n
+DIMENSION: 100\n
 EDGE_WEIGHT_TYPE: EXPLICIT\n
 EDGE_WEIGHT_FORMAT: UPPER_ROW\n
 EDGE_WEIGHT_SECTION\n""")
 for i in range (1, DIMENSION):
     for j in range (0, DIMENSION - i):
-        f.write("1 ")
+        if i < DIMENSION / 2:
+            f.write("1 ")
+        else:
+            f.write("2 ")
     f.write("\n")
 f.write("""
 EOF
