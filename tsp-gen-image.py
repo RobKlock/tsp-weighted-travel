7c122a4b00f1726d92aad82f2cd9007fb1f1d42d
RobKlock <rklock@oberlin.edu>
2021-05-07 00:20:57 -0400

    draws tour

--- tsp-gen-image.py
+++ tsp-gen-image.py
@@ -4,6 +4,7 @@ import random
 import os
 import shlex
 from datetime import datetime
+
 # Make the file
 cur_date = datetime.now()
 date_time = cur_date.strftime("%m-%d-%Y-%H-%M-%S")
@@ -12,7 +13,7 @@ filename = fi.format(date=date_time)
 f = open(filename, "x")
 # Dimension of grid, so the number of cities is DIMENSION ^ 2
 DIMENSION = 10
-TOURFILE = "test-tour.cyc"
+TOURFILE = "tour.cyc"
 
 # Dictionary of city coordinates
 cities = open('lin105.tsp', 'r')
@@ -24,7 +25,7 @@ for i in range(1, DIMENSION + 1):
         city_dictionary[city_num] = [i,j]
         # print(city_num)
         city_num += 1
-
+#print(city_dictionary)
 # Read tour file
 opt_tour = open(TOURFILE, "r")
 lines = opt_tour.readlines()
@@ -33,16 +34,11 @@ tour = []
 for line_index in range(1, len(lines)):
     tour.append(lines[line_index].split(" ")[0])
 print(tour)
-'''
-cities = {
 
-for line_index in range(6, len(cities_lines)):
-    city_index = ''
-    city_x = ''
-    city_y = ''
-    for i in cities_lines[line_index]:
-'''    
-# print(tour)
+coordinates = map(lambda city: city_dictionary[int(city)], tour)
+print(list(coordinates))
+tour_coordinates = list(coordinates)
+print(tour_coordinates)
 f.write("""%%BoundingBox: 0 0 200 200
 %!        %special comment (file is PostScript) \n0 0 moveto\nclosepath\n4 setlinewidth\nstroke\n""")
 # For each city in our tour, find its coordinates
@@ -58,6 +54,19 @@ for i in range (0, DIMENSION):
         #draw_circle = "{x} {y} 4 0 360 arc".format(x=i * 5, y = j * 5)
         #f.write(postScript_circle.format(x=i * 5, y = j * 5))
         f.write("""\nfill\nstroke\n""")
+f.write("\n%Draw Tour\n")
+f.write("\n2 setlinewidth\n")
+f.write("20 20 moveto\n")
+for i in map(lambda city: city_dictionary[int(city)], tour):
+    f.write(str(i[0] * 50 + 20))
+    f.write(" ")
+    f.write(str(i[1] * 50 + 20))
+    f.write(" lineto\n")
+    f.write("stroke\n")
+    f.write(str(i[0] * 50 + 20))
+    f.write(" ")
+    f.write(str(i[1] * 50 + 20))
+    f.write(" moveto\n")
 
 f.write("""showpage\n""")
 #os.system("open " + shlex.quote(filename))
